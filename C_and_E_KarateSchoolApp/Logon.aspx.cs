7b7b5b945803bddb3fba9f888a23f84b42c4e3e8
bdcarlson964 <150193546+bdcarlson964@users.noreply.github.com>
2023-11-19 17:19:39 -0600

    added logon class and linq to sql

---
+++ C_and_E_KarateSchoolApp/Logon.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace C_and_E_KarateSchoolApp
+{
+    public partial class Logon : System.Web.UI.Page
+    {
+        KarateDataContext dbcon;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+
+        }
+    }
+}
