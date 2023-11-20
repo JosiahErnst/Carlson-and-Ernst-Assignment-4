8bc0f004f0cf7b16b569e7f8e0f28efd9849e585
bdcarlson964 <150193546+bdcarlson964@users.noreply.github.com>
2023-11-20 17:05:13 -0600

    member page completed

--- C_and_E_KarateSchoolApp/Member.aspx
+++ C_and_E_KarateSchoolApp/Member.aspx
@@ -2,4 +2,15 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+        <br />
+        Name:
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </p>
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
+    <p>
+    </p>
+    <p>
+    </p>
 </asp:Content>
--- C_and_E_KarateSchoolApp/Member.aspx.cs
+++ C_and_E_KarateSchoolApp/Member.aspx.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using static System.Collections.Specialized.BitVector32;
 
 namespace C_and_E_KarateSchoolApp
 {
     public partial class Member : System.Web.UI.Page
     {
+        KarateDataContext dbcon;
+        string conn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\bdcar\\Downloads\\KarateSchool.mdf;Integrated Security=True;Connect Timeout=30";
         protected void Page_Load(object sender, EventArgs e)
         {
+            dbcon = new KarateDataContext(conn);
             if (Session.Count != 0)
             {
-                if (HttpContext.Current.Session["userType"].ToString().Trim() == "Instructor" || 
+                if (HttpContext.Current.Session["userType"].ToString().Trim() == "Instructor" ||
                     HttpContext.Current.Session["userType"].ToString().Trim() == "Administrator")
                 {
                     Session.Clear();
@@ -27,6 +32,32 @@ namespace C_and_E_KarateSchoolApp
 
             }
 
+            Member myUser = (from x in dbcon.Members
+                                 where x.Member_UserID == Convert.ToInt32(HttpContext.Current.Session["userID"])
+                                 select x).First();
+
+            if (myUser != null)
+            {
+                string firstName = myUser.MemberFirstName;
+                string lastName = myUser.MemberLastName;
+                Label1.Text = firstName + " " + lastName;
+                var records =
+                    from section in dbcon.Sections
+                    join instructor in dbcon.Instructors on section.Instructor_ID equals instructor.InstructorID
+                    join member in dbcon.Members on section.Member_ID equals HttpContext.Current.Session["userID"]
+                    select new
+                    {
+                        InstructorFirstName = instructor.InstructorFirstName,
+                        InstructorLastName = instructor.InstructorLastName,
+                        SectionName = section.SectionName,
+                        SectionStartDate = section.SectionStartDate,
+                        SectionFee = section.SectionFee
+                    };
+
+                GridView1.DataSource = records;
+                GridView1.DataBind();
+                GridView1.Visible = true;
+            }
         }
     }
 }
